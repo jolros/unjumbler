4fa03e861e16d986d298e93771d3ec3308ddba55
Joel Rosenberg <joeldrosenberg@yahoo.com>
2014-01-24 01:39:55 -0800

    Shortening string code

--- src/main/clojure/unjumbler/core.clj
+++ src/main/clojure/unjumbler/core.clj
@@ -1,11 +1,12 @@
 (ns unjumbler.core
-  (:require [clojure.math.combinatorics :as combo]))
+  (:require [clojure.math.combinatorics :as combo]
+            [clojure.string :as str]))
 
 (defn findwords
   [& words]
   (map (fn [word]
          (filter #(unjumbler.WordChecker/check %)
                  (map #(apply str %)
-                      (combo/permutations (seq (clojure.string/lower-case word))))))
+                      (combo/permutations (seq (str/lower-case word))))))
        words)
   )
